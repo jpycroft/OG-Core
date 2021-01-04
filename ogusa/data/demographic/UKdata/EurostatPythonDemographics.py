7f1aec90f8caed34ccf2b25068a637bf25ea36b2
Jonathan Pycroft <jonathan.pycroft@outlook.com>
2021-01-04 09:08:28 +0100

    add code to get demographic data from Eurostat

---
+++ ogusa/data/demographic/UKdata/EurostatPythonDemographics.py
@@ -0,0 +1,23 @@
+'''
+Trial download of Eurostat demographic data using the Eurostat Python Package
+https://pypi.org/project/eurostat/
+'''
+import eurostat
+
+###### USER ENTRY: country and year ########
+Country = 'UK'
+Year = 2018
+############################################
+
+StartPeriod = Year
+EndPeriod = Year
+
+filter_pars = {'GEO': [Country]}
+df_pop = eurostat.get_sdmx_data_df('demo_pjan', StartPeriod, EndPeriod, filter_pars, flags = True, verbose=True)
+df_mort = eurostat.get_sdmx_data_df('demo_magec', StartPeriod, EndPeriod, filter_pars, flags = True, verbose=True)
+df_fert = eurostat.get_sdmx_data_df('demo_fasec', StartPeriod, EndPeriod, filter_pars, flags = True, verbose=True)
+
+print(df_pop)
+print(df_mort)
+print(df_fert)
+
